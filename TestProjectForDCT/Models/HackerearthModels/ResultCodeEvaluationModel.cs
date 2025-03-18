f4ac57f34bc7c52a7f6e176eaa397da4365f1f44
Романюк Валентин <valentin.romaniuk.2004@gmail.com>
2025-03-18 18:15:22 +0200

    added service for work with Hackerearth API

---
+++ TestProjectForDCT/Models/HackerearthModels/ResultCodeEvaluationModel.cs
@@ -0,0 +1,11 @@
+﻿using TestProjectForDCT.Models.HackerearthModels.GenericModels;
+
+namespace TestProjectForDCT.Models.HackerearthModels;
+
+class ResultCodeEvaluationModel
+{
+    public RequestStatus request_status { get; set; }
+    public string he_id { get; set; }
+    public string status_update_url { get; set; }
+    public string context { get; set; }
+}
---
+++ TestProjectForDCT/Services/CodeEvaluationService.cs
@@ -0,0 +1,52 @@
+﻿using Newtonsoft.Json;
+using System.Net.Http;
+using TestProjectForDCT.Helpers;
+using TestProjectForDCT.Models.HackerearthModels;
+
+namespace TestProjectForDCT.ViewModels;
+
+class CodeEvaluationService
+{
+    private readonly HttpClient httpClient;
+
+    public CodeEvaluationService(IHttpClientFactory httpClientFactory, Config config)
+    {
+        httpClient = httpClientFactory.CreateClient(config.httpClientHackerearthAPIName);
+    }
+
+    public async Task<ResponseCodeEvaluationModel> SendCodeEvaluation(PostCodeEvaluationModel model)
+    {
+        var data = model.ToDictionary();
+
+        var content = new FormUrlEncodedContent(data);
+
+        var response = await httpClient.PostAsync("", content);
+
+        if(!response.IsSuccessStatusCode)
+        {
+            throw new Exception("Error sending code evaluation: " + response.StatusCode);
+        }
+
+        var responseContent = await response.Content.ReadAsStringAsync();
+
+        var result = JsonConvert.DeserializeObject<ResponseCodeEvaluationModel>(responseContent);
+
+        return result;
+    }
+
+    public async Task<ResultCodeEvaluationModel> GetResultCodeEvaluation(string he_id)
+    {
+        var response = await httpClient.GetAsync(he_id);
+
+        if (!response.IsSuccessStatusCode)
+        {
+            throw new Exception("Error getting code evaluation result: " + response.StatusCode);
+        }
+
+        var responseContent = await response.Content.ReadAsStringAsync();
+
+        var result = JsonConvert.DeserializeObject<ResultCodeEvaluationModel>(responseContent);
+
+        return result;
+    }
+}
